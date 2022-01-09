a6245d0484a32f6ffaf4a71bf8a559051406bd20
Chan0930 <abc970930@naver.com>
2022-01-09 21:55:29 +0900

    commit

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
 print("Hello World")
+def ps(x,y):
+    z=2x+3y
+    a=0
+    
+    for i in range(1,100):
+        if i%3==0:
+            continue
+        else:
+            a=a+i
+    return a*z
+
+ps(3,7)
